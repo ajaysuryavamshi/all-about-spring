d7452ae1085f203e2c4aa3964cd150c459ee0afa
ajaysuryavamshi <suryavamshiajay@yahoo.in>
2025-04-17 15:29:41 +0530

    [CI SKIP] Added basic setup

---
+++ src/main/java/com/automation/AllAboutSpringApplication.java
@@ -0,0 +1,13 @@
+package com.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllAboutSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllAboutSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automation/AllAboutSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllAboutSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
